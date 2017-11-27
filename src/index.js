5d4114477b027b2a623e7de0a0620207aa5bdced
Evileddie666 <32750844+Evileddie666@users.noreply.github.com>
2017-11-27 15:07:38 -0700

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,49 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Hello() {
+return (
+    <div>Hello</div>
+);
+}
+
+
+function ValidIndicator() {
+    var isValid = true;
+    return (
+       
+    <span>
+    { /* here is a comment */}
+    {isValid ? 'valid' : 'not valid'}</span>
+    );
+    }
+
+    
+
+function World() {
+    return  1+2;
+    /* here is a comment */
+    }
+    
+function SubmitButton()
+{
+    var buttonLablel = "Submit";
+    return(
+        /* here is a comment */
+        <button>{buttonLablel}</button>
+    )
+}
+
+    function HelloWorld() {
+        return (
+         <div>
+             <Hello/>  <World/>
+             <ValidIndicator/>
+             
+        </div>
+        );
+        }
+        
+/* here is a comment */
+ReactDOM.render(<HelloWorld/>,document.querySelector('#root'));
